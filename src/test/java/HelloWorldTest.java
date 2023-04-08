f2bf46a4a933ca35f29a27825b2097508dd3779c
Konstantin200889 <k.kapon@mail.ru>
2023-04-08 19:19:25 +0300

    HelloWorldTest update 6/04

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,112 +16,101 @@ import java.util.Map;
 import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class HelloWorldTest {
 
     private final static String testUrl = "https://playground.learnqa.ru/api";
 
-    /*@Test
-    public void test1() {
-                JsonPath response =  RestAssured
-                .get(testUrl + "/get_json_homework")
-                        .jsonPath();
-                response.prettyPrint();
-
-        ArrayList allMessages = response.get("messages");
-        HashMap SecondMessage = (HashMap) allMessages.get(1);
-        String secondMessageText = response.get("messages[1].message");
-        Assertions.assertEquals(secondMessageText, "And this is a second message");
-        System.out.println(secondMessageText);
-    }*/
-
-
 
     /*@Test
-    public void test2() {
+    public void testRestAssured1() {
         Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get(testUrl + "/long_redirect")
+                .get(testUrl + "/map")
                 .andReturn();
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
-        Headers respHeaders = response.getHeaders();
-        System.out.println(respHeaders.getValue("Location").toString());
-    }
-    */
-
-
-
+                assertTrue(response.statusCode() == 200, "unexpected code");
+               //функция assertTrue приводит выражение в скобках к типу boolean
+    }*/
 
     /*@Test
-    public void test3() {
-        int i = 0;
-        String url = testUrl + "/long_redirect";
-        do {
-            Response response = RestAssured
-                    .given()
-                    .redirects()
-                    .follow(false)
-                    .when()
-                    .get(url)
-                    .andReturn();
-            int statusCode = response.getStatusCode();
-            Headers respHeaders = response.getHeaders();
-            if (statusCode == 200) {
-                break;
-            }
-            url = respHeaders.getValue("Location").toString();
-            i++;
-            *//*System.out.println(url);
-            System.out.println(statusCode);*//*
-        } while (true);
-        System.out.println(i);
-        System.out.println(url);
+    public void testRestAssured2() {
+        Response response = RestAssured
+                .get(testUrl + "/map1")
+                .andReturn();
+        assertEquals(200, response.statusCode(), "unexpected code");
+        //для сравнения результата сначала передаем ожидаемое значение, а затем действительное
     }*/
 
 
+    /*@Test
+    public void testRestAssured3() {
+        JsonPath response = RestAssured
+                .get(testUrl + "/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, someone", answer, "wrong response");
+        //делаем запрос без параметров
+        response.prettyPrint();
+    }*/
 
 
     /*@Test
-    public void test4() throws InterruptedException {
-        JsonPath response1 = RestAssured
-                .get(testUrl+ "/longtime_job")
-                .jsonPath();
-        String getToken = response1.get("token");
-        Integer getTime = response1.get("seconds");
-        HashMap<String, String> mapToken = new HashMap<>();
-        mapToken.put("token", getToken);
-        System.out.println(mapToken);
-        JsonPath response2 = RestAssured
+    public void testRestAssured4() {
+        String name = "Username";
+        //передаем в качестве параметра name = "Username" в теле теста
+        JsonPath response = RestAssured
                 .given()
-                .queryParams(mapToken)
-                .get(testUrl+ "/longtime_job")
+                .queryParams("name", name)
+                .get(testUrl + "/hello")
                 .jsonPath();
-                String fieldStatus = response2.get("status");
-                if (!Objects.equals(fieldStatus, "Job is NOT ready")) {
-                    System.out.println("wrong status");
-                }
-                else {
-                    System.out.println(fieldStatus);
-                }
-                Thread.sleep(getTime * 1000);
-                JsonPath response3 = RestAssured
-                        .given()
-                        .queryParams(mapToken)
-                        .get(testUrl+ "/longtime_job")
-                        .jsonPath();
-                String newFieldStatus = response3.get("status");
-                String fieldResult = response3.get("result");
-                Assertions.assertEquals(newFieldStatus, "Job is ready");
-                Assertions.assertNotNull(fieldResult, "field result is empty");
-                System.out.println(newFieldStatus);
-                System.out.println(fieldResult);
-
+        String answer = response.getString("answer");
+        assertEquals("Hello, " + name, answer, "wrong response");
+        response.prettyPrint();
     }*/
+
+
+
+    @ParameterizedTest // в @ParameterizedTest мы передаем набор параметров, тест будет запускаться столько раз, сколько параметров передано
+    @ValueSource(strings = {"", "John", "Pete"})// в этом теге передаем набор параметров
+    // мы передали 3 параметра в тест, теперь тест будет запущен 3 раза, каждый раз подставляя очередной параметр
+    public void testRestAssured5(String name) { // в String name будут по очереди передаваться значения из (strings = {"", "John", "Pete"})
+        Map <String, String> queryParams = new HashMap<>();
+
+        if(name.length() > 0) { // обрабатываем ситуацию, когда имя - пустое
+            queryParams.put("name", name); // добавляем имя в качестве параметра в созданную мапу queryParams
+        }
+
+       //чтобы автоматически передавать параметры в тест, мы добавили аннотацию @parametrizedTest + @ValueSource
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams) // поместили мапу queryParams в тело запроса
+                .get(testUrl + "/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone"; // конструкция тернарного оператора: переменная= (условие) ? результат_если_true : результат_если_false
+        //если длина значения переменной name больше нуля, то она сохранится в новую переменную expectedName
+        assertEquals("Hello, " + expectedName, answer, "wrong response");
+        response.prettyPrint();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/test/java/UserAuthTest.java
@@ -0,0 +1,54 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Headers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserAuthTest {
+
+
+@Test
+public void testAuthUser() {
+    Map<String, String> authData = new HashMap<>();
+    authData.put("email", "vinkotov@example.com");
+    authData.put("password", "1234");
+
+    Response responseGetAuth = RestAssured
+            .given()
+            .body(authData)
+            .post("https://playground.learnqa.ru/api/user/login")
+            .andReturn();
+
+            Map<String, String> cookies = responseGetAuth.getCookies(); // вытаскиваем из тела куки, заголовки, id пользователя
+            Headers headers = responseGetAuth.getHeaders();
+            int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+
+            assertEquals(200, responseGetAuth.statusCode(), "wrong status code");
+            assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookie");
+            assertTrue(headers.equals("x-csrf-token"), "Response doesn't have 'x-csrf-token' header");
+            assertTrue(responseGetAuth.jsonPath().getInt("user_id") > 0, "'user_id should be greater then 0'");
+
+            JsonPath responseCheckAuth = (JsonPath) RestAssured
+            .given()
+            .header("x-csrf-token", responseGetAuth.getHeader("x-csrf-token"))
+            .cookie("auth_sid", responseGetAuth.getCookie("auth_sid"))
+                    .get("https://playground.learnqa.ru/api/user/auth")
+                    .jsonPath();
+            int userIdOnCheck = responseCheckAuth.getInt("user_id");
+            assertTrue(userIdOnCheck > 0, "Unexpected user id" + userIdOnCheck);
+            assertEquals(userIdOnAuth, userIdOnCheck, "'userIdOnAuth' is not equals 'userIdOnCheck'");
+
+}
+
+
+
+
+
+
+}
